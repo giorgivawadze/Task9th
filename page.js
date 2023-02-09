72aa62a0d3487691df1dac4ae90398279c3111c7
giorgivawadze <116506482+giorgivawadze@users.noreply.github.com>
2023-02-09 18:45:38 +0400

    task 10

--- page.js
+++ page.js
@@ -3,6 +3,11 @@ const title = document.querySelector (".container,h1");
 const input = document.querySelector (".container,input");
 const button = document.querySelector (".container,button");
 const taskList = document.querySelector ("#taskList");
+const spinner = document.querySelector ('#spinner');
+const clearall = document.getElementById ('clearall');
+const cleardone = document.getElementById ('cleardone');
+const save = document.getElementById ('Save');
+const toDoItems = [];
 
 input.addEventListener('click', function () {
     let task = taskInput.value;
@@ -13,6 +18,58 @@ input.addEventListener('click', function () {
     taskList.appendChild (li);
     taskInput.value= "";
 }
-
-
 )
+
+function resetToDoList() {
+    taskList.innerHTML = "";
+    taskInput.value = "";
+    toDoItems = [];
+  }  
+ clearall.addEventListener("click", resetToDoList);
+
+ save.addEventListener("click", function() {
+    const toDoArray = Array.from(taskList.children).map(li => li.textContent);
+    localStorage.setItem("toDoItems", JSON.stringify(toDoArray));
+  });
+  
+  function loadToDoList() {
+    const savedToDos = localStorage.getItem("toDoItems");
+    if (savedToDos) {
+      const toDoArray = JSON.parse(savedToDos);
+      toDoArray.forEach(toDo => {
+        let li = document.createElement("li");
+        li.textContent = toDo;
+        taskList.appendChild(li);
+      });
+    }
+  }
+  
+  loadToDoList();
+
+  cleardone.addEventListener("click", function() {
+    const doneTasks = taskList.querySelectorAll("li.done");
+    doneTasks.forEach(li => {
+      li.remove();
+    });
+  
+    const toDoArray = Array.from(taskList.children).map(li => li.textContent);
+    localStorage.setItem("toDoItems", JSON.stringify(toDoArray));
+  });
+  
+  taskList.addEventListener("click", function(event) {
+    if (event.target.tagName === "LI") {
+      event.target.classList.toggle("done");
+    }
+  });
+  
+
+function init () {
+    spinner.style.display = 'block';
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then ((res) => res.json ())
+    .then ((res) => {
+         spinner.style.display = 'none';
+         console.log(res.data);
+    })
+} 
+init ()
